86913a81a8694ea118361afc8346638e40561807
Eric Greene <eric@t4d.io>
2020-06-15 20:57:34 -0400

    add car tool lab solutions

---
+++ demo-app/src/app/car-tool/components/car-home/car-home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Car } from '../../models/Car';
+
+@Component({
+  selector: 'app-car-home',
+  templateUrl: './car-home.component.html',
+  styleUrls: ['./car-home.component.css']
+})
+export class CarHomeComponent implements OnInit {
+
+  headerText = 'Car Tool';
+
+  cars: Car[] = [
+    { id: 1, make: 'Ford', model: 'Fusion Hybrid', year: 2020, color: 'blue', price: 45000 },
+    { id: 2, make: 'Tesla', model: 'S', year: 2019, color: 'red', price: 120000 },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
